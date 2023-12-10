65909e0f61f41045289bca3e9bc325df64707d3d
Lino Alvarez <l.alvarez@amsacs.org>
2023-12-09 22:13:57 -0500

    Update page-planner3.php
    Updated <title>

--- page-planner3.php
+++ page-planner3.php
@@ -4,19 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alvarez WP 23_24 Planner (211) 3.0 V</title>
+    <title><?php echo $owner . ' ' . $schoolYear .  ' ('. $ownerBaseRoom . ')'; ?> A5 Size</title>
     <?php include('my-style3.php') ?>   
 </head>
 <body>
 <div class="letterV"></div>    
 <!-- <div class="letterH"></div>     -->
 <div class="wrapper">
-    
     <div class="page schedule" id="schedule">
-        
         <div class="owner"> <h1> <?php echo '<span>' . $owner . '</span> <span>' . $schoolYear . '</span> <span>' . '('. $ownerBaseRoom . ')</span>'; ?></h1>
         </div>
-                
         <div class="grid-schedule">
             <div class="wide day-header"><?php foreach ($dayHeaders as $dayHeader) {echo'<div>' . $dayHeader . '</div>'; } ?></div>
             <div class="class-times"><?php foreach ($periods as $period) { echo '<div class="class-time"><span>' . $period . '</span></div>'; }?></div>
@@ -29,47 +26,28 @@
             <div class="day7"><?php foreach ($day7 as $key => $value){echo'<div class="'.$key.'">'.'<span class="block">'.$key.'</span>'.'<span class="class">'.$value.'</span>'.'</div>';}?></div>
             <div class="day8"><?php foreach ($day8 as $key => $value){echo'<div class="'.$key.'">'.'<span class="block">'.$key.'</span>'.'<span class="class">'.$value.'</span>'.'</div>';}?></div>
         </div>
-        
         <div class="lunch-times"><span>11:11 - </span><b>L1</b><span> - 11:33 - </span><b>L2</b><span> - 11:56 - </span><b>L3</b><span> - 12:18</span></div>
-        
     </div> <!-- page -->
-
     <div class="page y2324 wide pb0" id="y2324">
         <!-- <h1>page</h1> -->
         <iframe width="100%" height="1000px" src="https://www.linoalvarez.org/calendario7" frameborder="0"></iframe>
     </div> <!-- page -->
-
         <div class="page two-columns wide no-school-important-dates">
-
             <div class="no-school" id="no-school">
-                
                 <h1 class="wide center">No School</h1>
-                
                 <?php foreach ($noSchoolNew as $key => $value)  { echo "<div> <span><a href='#f" . $key . "'>" . date("M d Y", strtotime($key)) .  "</a></span>" . "<span>" . $value . "</span></div>"; }; ?>
             </div>
-
             <div class="important-dates">
-            
                 <h1 class="center">Important Dates</h1>
-                    
                 <?php foreach($lectivoPeroSpecial as $key => $value) { echo  '<div class="' . $value . '"><span><a href="#f' . $key . '">' . date("D M d Y", strtotime($key)) . '</a></span><span class="'  . $value . '"' . '>'.'<span>' . $value . '</span></div>' ; }; ?>
-                    
             </div>
         </div> <!-- page -->
-
-
 <?php $schoolday = 1; $amsaDay = 1;
-    
     while (strtotime($date) <= strtotime($end_date)) {
         $timestamp = strtotime($date);
-
         $day = date('l', $timestamp);
-
             if(!in_array($date, $noSchoolNewTest)) { 
-
                 if($day != "Saturday" && $day != "Sunday" ) { ?> 
-      
-      
 <?php   $fecha = $date; ?>
 <?php   $fechaAmsaDay = " Day " . $amsaDay ?>
 <?php   $weekDay =  $day ?>
