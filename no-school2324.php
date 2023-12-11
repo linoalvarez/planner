7d079ea2f979a2c189dc16013aa38524c867056f
Lino Alvarez <l.alvarez@amsacs.org>
2023-12-11 18:28:58 -0500

    Add files via upload
    Mixed in with the 180 school days.

---
+++ no-school2324.php
@@ -0,0 +1,106 @@
+<?php
+    //no school 2023 - 2024 School Year
+
+    $noSchool = array( 
+        '2023-09-04', // Labor Day
+        '2023-10-09', // Indigenous Peoples' Day
+        '2023-11-10', // Veteran's Day
+        '2023-11-23', // Thanksgiving Break
+        '2023-11-24', // Thanksgiving Break
+        '2023-12-18', // December Break 
+        '2023-12-19', // December Break 
+        '2023-12-20', // December Break 
+        '2023-12-21', // December Break 
+        '2023-12-22', // December Break 
+        '2023-12-25', // December Break 
+        '2023-12-26', // December Break 
+        '2023-12-27', // December Break 
+        '2023-12-28', // December Break 
+        '2023-12-29', // December Break 
+        '2024-01-01', // December Break
+        '2024-01-15', // MLK Day 
+        '2024-02-19', // Presidents' Day 
+        '2024-02-20', // February Break 
+        '2024-02-21', // February Break 
+        '2024-02-22', // February Break 
+        '2024-02-23', // February Break 
+        '2024-04-15', // Patriots' Day 
+        '2024-04-16', // April Break 
+        '2024-04-17', // April Break 
+        '2024-04-18', // April Break 
+        '2024-04-19', // April Break 
+        '2024-05-27', // Memorial Day 
+        '2024-06-19'  // Juneteenth 
+    );
+
+
+    $noSchoolNames = array( 
+        ' Labor&nbsp;Day',                       //2023-09-04
+        ' Indigenous&nbsp;People\'s&nbsp;Day',   //2023-10-09
+        ' Veterans\'&nbsp;Day',                  //2023-11-10
+        ' Thanksgiving&nbsp;Break',              //2023-11-23
+        ' Thanksgiving&nbsp;Break',              //2023-11-24
+        ' December&nbsp;Break',                  //2023-12-18
+        ' December&nbsp;Break',                  //2023-12-19
+        ' December&nbsp;Break',                  //2023-12-20
+        ' December&nbsp;Break',                  //2023-12-21
+        ' December&nbsp;Break',                  //2023-12-22
+        ' December&nbsp;Break',                  //2023-12-25
+        ' December&nbsp;Break',                  //2023-12-26
+        ' December&nbsp;Break',                  //2023-12-27
+        ' December&nbsp;Break',                  //2023-12-28
+        ' December&nbsp;Break',                  //2023-12-29
+        ' December&nbsp;Break',                  //2024-01-01
+        ' MLK\'s&nbsp;Day',                      //2024-01-15
+        ' Presidents\'&nbsp;Day',                //2024-02-19
+        ' February&nbsp;Break',                  //2024-02-20
+        ' February&nbsp;Break',                  //2024-02-21
+        ' February&nbsp;Break',                  //2024-02-22
+        ' February&nbsp;Break',                  //2024-02-23
+        ' Patriots\'&nbsp;Day',                  //2024-04-15
+        ' April&nbsp;Break',                     //2024-04-16
+        ' April&nbsp;Break',                     //2024-04-17
+        ' April&nbsp;Break',                     //2024-04-18
+        ' April&nbsp;Break',                     //2024-04-19
+        ' Memorial&nbsp;Day',                    //2024-05-27
+        ' Juneteenth'                            //2024-06-19
+    );
+
+
+
+
+ $noSchoolNew = array( 
+        '2023-09-04' =>  'Labor Day',
+        '2023-10-09' =>  'Indigenous\' Peoples Day',
+        '2023-11-10' =>  'Veterans\' Day',
+        '2023-11-23' =>  'Thanksgiving Break',
+        '2023-11-24' =>  'Thanksgiving Break',
+        '2023-12-18' =>  'December Break Start',
+        '2023-12-19' =>  'December Break',
+        '2023-12-20' =>  'December Break',
+        '2023-12-21' =>  'December Break',
+        '2023-12-22' =>  'December Break',
+        '2023-12-25' =>  'December Break',
+        '2023-12-26' =>  'December Break',
+        '2023-12-27' =>  'December Break',
+        '2023-12-28' =>  'December Break',
+        '2023-12-29' =>  'December Break',
+        '2024-01-01' =>  'December Break End',
+        '2024-01-15' =>  'MLK\'s Day',
+        '2024-02-19' =>  'February Break Start',
+        '2024-02-20' =>  'February Break',
+        '2024-02-21' =>  'February Break',
+        '2024-02-22' =>  'February Break',
+        '2024-02-23' =>  'February Break End',
+        '2024-04-15' =>  'April Break Start',
+        '2024-04-16' =>  'April Break',
+        '2024-04-17' =>  'April Break',
+        '2024-04-18' =>  'April Break',
+        '2024-04-19' =>  'April Break End',
+        '2024-05-27' =>  'Memorial Day',
+        '2024-06-19' =>  'Juneteenth',
+    );
+
+
+
+?>
