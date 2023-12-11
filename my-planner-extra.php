483e8445b6051993a4a9cad56ed8526f0c7f004a
Lino Alvarez <l.alvarez@amsacs.org>
2023-12-11 18:42:43 -0500

    Add files via upload
    Planner extras

---
+++ my-planner-extra.php
@@ -0,0 +1,119 @@
+    <div class="day pb0 wide" id="y2023">
+        <iframe width="100%" height="750px" src="https://www.linoalvarez.org/calendario2" frameborder="0"></iframe>
+    </div>
+
+    <!-- <div class="day wide pb0 noPrint" id="y2024"> </div> -->
+
+    <div class="day pb0 wide" id="y2024">
+        <iframe width="100%" height="750px" src="https://www.linoalvarez.org/calendario3" frameborder="0"></iframe>
+    </div>
+
+    <!-- <div class="day pb0 wide" id="y202324"> -->
+        <!-- <iframe width="100%" height="750px" src="https://www.linoalvarez.org/calendario4" frameborder="0"></iframe> -->
+    <!-- </div> -->
+
+    <div class="page wide mapamundi pb0 invert" id="mapa1">
+        <h1 class="center">&nbsp;</h1>
+        <img src="https://linoalvarez.org/wp-content/themes/twentynineteen/mapaMundi.jpg" alt="">
+        <h1 class="center">&nbsp;</h1>
+    </div>
+
+    <div class="page wide pb0 mapamundi" id="mapa2">
+        <h1 class="center">&nbsp;</h1>
+        <img src="https://linoalvarez.org/wp-content/themes/twentynineteen/mapaMundiColor.jpg" alt="">
+        <h1 class="center">&nbsp;</h1>
+    </div>
+
+    <div class="page wide pb0 mapamundi" id="mapa3">
+        <img src="https://linoalvarez.org/wp-content/themes/twentynineteen/mapaEuropi.jpg" alt="">
+    </div>
+
+    <div class="page wide pb0 mapamundi" id="mapa4">
+        <img src="https://linoalvarez.org/wp-content/themes/twentynineteen/flagposter.jpg" alt="">
+    </div>
+
+    <div class="page wide">
+        <h1 class="center">Rosters</h1>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+
+    <div class="page wide">
+        <h1 class="center">Sitting Charts</h1>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+
+    <div class="page wide pb0 telefonos" id="telefonos">
+        <h1>Extensions</h1>
+        <ul class="no-bullets">
+            <li><span><h2>Name</h2></span><span><h2>ext</h2></span></li>
+            <li><span><h2>Name</h2></span><span><h2>ext</h2></span></li>
+            <li><span><h2>Name</h2></span><span><h2>ext</h2></span></li>
+            <li><span>8th Grade Counselors</span><span></span></li>
+            <li><span>IT</span><span></span></li>
+            <li><span>Front Desks</span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span>Erica</span><span></span></li>
+            <li><span>199</span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span>Joe</span><span></span></li>
+            <li><span>201</span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span>Joanne</span><span></span></li>
+            <li><span>164</span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+            <li><span></span><span></span></li>
+        </ul>
+    </div>
+
+    <div class="page wide pb0 portada" id="portada">
+        <h1>2023/24</h1>
+    </div>
---
+++ my-rosters.php
@@ -0,0 +1,36 @@
+        <div class= "page wide">
+            <h1 class="center">Roster A2</h1>
+        </div>
+        
+        <div class= "page wide">
+            <h1 class="center">Roster B1</h1>
+        </div>    
+
+        <div class= "page wide">
+            <h1 class="center">Roster C1</h1>
+        </div>    
+
+        <div class= "page wide">
+            <h1 class="center">Roster C2</h1>
+        </div>    
+
+        <div class= "page wide">
+            <h1 class="center">Roster D1</h1>
+        </div>        
+
+        <div class= "page wide">
+            <h1 class="center">Roster D2</h1>
+        </div>    
+
+        <div class= "page wide">
+            <h1 class="center">Roster E2</h1>
+        </div>    
+        
+        <div class= "page wide">
+            <h1 class="center">Roster F2</h1>
+        </div>    
+        
+        <div class= "page wide">
+            <h1 class="center">Roster G1</h1>
+        </div>            
+
